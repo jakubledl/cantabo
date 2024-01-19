ce473bfaf189e3a37e1790fac6ffdb7c7a22739f
Jakub Lédl <jakubledl@gmail.com>
2024-01-19 20:31:07 +0100

    fix(deps): add sveltestrap

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,21 @@
 <script lang="ts">
+  import {
+    Container,
+    Col,
+    Row,
+    Styles,
+  } from "@sveltestrap/sveltestrap";
 </script>
 
-<p>hello</p>
+<Styles icons={false} />
+
+<Container>
+  <Row>
+    <Col xxl={{ size: 6, offset: 3 }}>
+      <h1>Asdf</h1>
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer vulputate sem a nibh rutrum consequat. Nullam rhoncus aliquam metus. Aenean id metus id velit ullamcorper pulvinar. Proin pede metus, vulputate nec, fermentum fringilla, vehicula vitae, justo. Vestibulum fermentum tortor id mi. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Morbi imperdiet, mauris ac auctor dictum, nisl ligula egestas nulla, et sollicitudin sem purus in lacus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque tincidunt scelerisque libero. Integer tempor. Aenean placerat. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Etiam commodo dui eget wisi. Suspendisse sagittis ultrices augue. Curabitur vitae diam non enim vestibulum interdum. Morbi leo mi, nonummy eget tristique non, rhoncus non leo. Aenean placerat. Fusce consectetuer risus a nunc.</p>
+    </Col>
+  </Row>
+</Container>
+
+<style src="./app.css"></style>
